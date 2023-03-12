8b2165dd60c67b0c93d3e4ca40c19bf42778dc3c
MAlazhariy <mostafa.maok2000@gmail.com>
2023-03-12 22:56:29 +0200

    modify: handle init DI & routerHelper

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
+import 'package:ramadan_kareem/helpers/router_helper.dart';
 import 'package:ramadan_kareem/my_app.dart';
 import 'package:ramadan_kareem/providers/auth_provider.dart';
 import 'package:ramadan_kareem/providers/internet_provider.dart';
@@ -15,8 +16,15 @@ void main() async {
   // set device orientations
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
+  // init routes
+  RouterHelper.init();
+
+  // init singleton
+  await Di.init();
+
   // init Firebase
   await Firebase.initializeApp();
+
   runApp(
     MultiProvider(
       providers: [
