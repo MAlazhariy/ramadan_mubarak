223ea6151514175d2ec7d2fc7cdef115aebd1f6b
MAlazhariy <mostafa.maok2000@gmail.com>
2023-03-27 06:46:45 +0200

    add: facebook page to more menu

--- lib/utils/app_uri.dart
+++ lib/utils/app_uri.dart
@@ -5,4 +5,6 @@ class AppUri {
   static const FCM_BASE_URL  = "https://fcm.googleapis.com";
   static const SEND_FCM  = "/fcm/send";
   static const ADMIN_FCM_TOPIC = "admin";
+
+  static const FACEBOOK_PAGE = "https://www.facebook.com/ed3ely.m3ak";
 }
--- lib/view/more/more_screen.dart
+++ lib/view/more/more_screen.dart
@@ -1,9 +1,13 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:ramadan_kareem/helpers/push_to.dart';
+import 'package:ramadan_kareem/utils/app_uri.dart';
 import 'package:ramadan_kareem/view/base/internet_consumer_builder.dart';
 import 'package:ramadan_kareem/view/more/base/more_item_builder.dart';
+import 'package:ramadan_kareem/view/screens/settings/settings_screen.dart';
 import 'package:ramadan_kareem/view/screens/settings/update_user_screen.dart';
 import 'package:sizer/sizer.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class MoreScreen extends StatefulWidget {
   const MoreScreen({Key? key}) : super(key: key);
@@ -34,6 +38,16 @@ class _MoreScreenState extends State<MoreScreen> {
                   icon: Icons.edit_outlined,
                   onTap: () {
                     pushTo(context, const UpdateUserDataScreen());
+                  },
+                ),
+                MoreItemBuilder(
+                  title: 'صفحتنا على فيسبوك',
+                  icon: Icons.facebook_outlined,
+                  onTap: () async {
+                    await launchUrlString(
+                      AppUri.FACEBOOK_PAGE,
+                      mode: LaunchMode.externalNonBrowserApplication,
+                    );
                   },
                 ),
               ],
