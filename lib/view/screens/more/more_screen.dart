65c103f73255268506aa0a7e464ca0ceefaee4e6
MAlazhariy <mostafa.maok2000@gmail.com>
2023-03-27 10:30:56 +0200

    modify: handle more screen

--- lib/view/screens/more/more_screen.dart
+++ lib/view/screens/more/more_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:ramadan_kareem/helpers/push_to.dart';
+import 'package:ramadan_kareem/providers/profile_provider.dart';
 import 'package:ramadan_kareem/utils/app_uri.dart';
 import 'package:ramadan_kareem/view/base/internet_consumer_builder.dart';
+import 'package:ramadan_kareem/view/screens/admin/new_members_screen.dart';
 import 'package:ramadan_kareem/view/screens/more/base/more_item_builder.dart';
 import 'package:ramadan_kareem/view/screens/settings/settings_screen.dart';
 import 'package:ramadan_kareem/view/screens/update_user/update_user_screen.dart';
@@ -31,26 +34,39 @@ class _MoreScreenState extends State<MoreScreen> {
         ),
         child: InternetConsumerBuilder(
           builder: (context, internetProvider) {
-            return ListView(
-              children: [
-                MoreItemBuilder(
-                  title: 'تعديل الدعاء',
-                  icon: Icons.edit_outlined,
-                  onTap: () {
-                    pushTo(context, const UpdateUserDataScreen());
-                  },
-                ),
-                MoreItemBuilder(
-                  title: 'صفحتنا على فيسبوك',
-                  icon: Icons.facebook_outlined,
-                  onTap: () async {
-                    await launchUrlString(
-                      AppUri.FACEBOOK_PAGE,
-                      mode: LaunchMode.externalNonBrowserApplication,
-                    );
-                  },
-                ),
-              ],
+            return Consumer<ProfileProvider>(
+              builder: (context, profileProvider, _) {
+                return ListView(
+                  children: [
+                    if (profileProvider.userDetails != null)
+                      MoreItemBuilder(
+                        title: 'تعديل الدعاء',
+                        icon: Icons.edit_outlined,
+                        onTap: () {
+                          pushTo(context, const UpdateUserDataScreen());
+                        },
+                      ),
+                    MoreItemBuilder(
+                      title: 'صفحتنا على فيسبوك',
+                      icon: Icons.facebook_outlined,
+                      onTap: () async {
+                        await launchUrlString(
+                          AppUri.FACEBOOK_PAGE,
+                          mode: LaunchMode.externalNonBrowserApplication,
+                        );
+                      },
+                    ),
+                    if (profileProvider.userDetails?.isModerator == true)
+                      MoreItemBuilder(
+                        title: 'قبول الأسماء',
+                        icon: Icons.manage_accounts_outlined,
+                        onTap: () async {
+                          pushTo(context, const NewMembersScreen());
+                        },
+                      ),
+                  ],
+                );
+              },
             );
           },
           noInternetScreen: (context, internetProvider) {
