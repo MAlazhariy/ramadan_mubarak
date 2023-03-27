dce6752e0172a1469bae0fa825cbf51a171c2677
MAlazhariy <mostafa.maok2000@gmail.com>
2023-03-27 06:11:19 +0200

    modify: field doaa provider, add clear

--- lib/providers/field_doaa_provider.dart
+++ lib/providers/field_doaa_provider.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 
 class FieldDoaaProvider extends ChangeNotifier {
-
   final _maxDoaaLength = 200;
   int _doaaLength = 0;
   _Field? _loginField;
@@ -9,20 +8,31 @@ class FieldDoaaProvider extends ChangeNotifier {
   int get maxDoaaLength => _maxDoaaLength;
 
   int get doaaLength => _doaaLength;
+
   bool get isInNameField => _loginField == _Field.name;
+
   bool get isInDoaaField => _loginField == _Field.doaa;
 
-  void onChangeDoaaLength(int length){
-    if(!isInDoaaField){
+  void onChangeDoaaLength(int length) {
+    if (!isInDoaaField) {
       onTapField(isName: false);
     }
     _doaaLength = length;
     notifyListeners();
   }
 
-  void onTapField({required bool isName}){
+  void onTapField({required bool isName}) {
     _loginField = isName ? _Field.name : _Field.doaa;
     notifyListeners();
+  }
+
+  void clear() {
+    _doaaLength = 0;
+    _loginField = null;
+  }
+
+  void initDoaaLength(String doaa){
+    _doaaLength = doaa.length;
   }
 }
 
